fa4ae4cd29c89d8080fbbbf7f397d8ddd38ddc56
akashfuldeore <akashfuldeore05@gmail.com>
2023-10-21 11:38:41 +0530

    added remove duplicates code

---
+++ removeDuplicates.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class removeDuplicates {
+    public static void main(String[] args) {
+        List<String> name = Arrays.asList("Akash", "Akash", "Prasad" , "Satish", "Fuldeore", "Sumit");
+
+        List<String> uniqueName = name.stream().distinct().collect(Collectors.toList());
+        System.out.println(uniqueName);
+
+        //Different Method to remove Duplicates
+        name.stream().distinct().forEach(System.out::println);
+    }
+}
